9fb00112f4c821a99abbf9eb3fbb87a567fe60ef
Leonardo Rivera <leodrivera@gmail.com>
2021-06-20 12:06:07 -0300

    Add touch sensor part to the code

--- blink_heart/blink_heart_code/blink_heart_code.ino
+++ blink_heart/blink_heart_code/blink_heart_code.ino
@@ -3,59 +3,67 @@ This program was based on the Busy-Wait approach provided by Microchip Technolog
  */
 
 //BPM: Numbers of batiments per minute
-const int BPM=60;
+#define  BPM 60
 
 //PWM: Define de value for maximum light intensity (0~100)
-const int PWM=80;
+#define  PWM 80
 
-//Define PWM led pins. In the case of Attiny85, we have 3 pins: 0,1,3
-const int pins[3] = {0,1,3};
+//Define PWM led pins. In the case of Attiny85, we have 3 pins: 0,1,4
+const int pins[3] = {0,1,4};
+
+//Define pin for using the touch
+#define  pin_touch 2
 
 //Calculate de size of the pins array
 const int len_pins = sizeof(pins)/sizeof(pins[0]);
 
 //small_delay: delay used inside each step
-const int SMALL_DELAY=7;
+#define SMALL_DELAY 7
 
 //get beats interval -> 1 BPM = 1 beat/(60*1000) ms
 const int BEATS_INTERVAL=60000/BPM;
 
 //get sleep_interval -> 1 BPM = 1 beat/(60*1000) ms
-const long SLEEP_INTERVAL = BEATS_INTERVAL-80*SMALL_DELAY;
+const int SLEEP_INTERVAL = BEATS_INTERVAL-80*SMALL_DELAY;
 
 void setup() {
   for(uint8_t j=0; j<len_pins; j++) {
     pinMode(pins[j], OUTPUT);
   }
+  pinMode(pin_touch, INPUT);
 }
 
 void loop() {
-  for(uint8_t i=0; i<15; i++) {
-      for(uint8_t j=0; j<len_pins; j++) {
-        analogWrite(pins[j],((float)i/31)*((float)PWM/100)*255);
-      }
-      delay(SMALL_DELAY);
-  }
-
-    for(uint8_t i=15; i>5; i--) {
-      for(uint8_t j=0; j<len_pins; j++) {
-        analogWrite(pins[j],((float)i/31)*((float)PWM/100)*255);
+  int touchState = digitalRead(pin_touch);
+  
+  if(touchState == HIGH){
+    for(uint8_t i=0; i<15; i++) {
+        for(uint8_t j=0; j<len_pins; j++) {
+          analogWrite(pins[j],((float)i/31)*((float)PWM/100)*255);
         }
         delay(SMALL_DELAY);
+    }
+  
+      for(uint8_t i=15; i>5; i--) {
+        for(uint8_t j=0; j<len_pins; j++) {
+          analogWrite(pins[j],((float)i/31)*((float)PWM/100)*255);
+          }
+          delay(SMALL_DELAY);
+    }
+  
+    for(uint8_t i=5; i<30; i++) {
+        for(uint8_t j=0; j<len_pins; j++) {
+          analogWrite(pins[j],((float)i/31)*((float)PWM/100)*255);
+          }
+          delay(SMALL_DELAY);
+    }
+  
+    for(uint8_t i=31; i>0; i--) {
+        for(uint8_t j=0; j<len_pins; j++) {
+          analogWrite(pins[j],(((float)i-1.0)/31)*((float)PWM/100)*255);
+          }
+          delay(SMALL_DELAY);
+    }
+    delay(SLEEP_INTERVAL);
   }
-
-  for(uint8_t i=5; i<30; i++) {
-      for(uint8_t j=0; j<len_pins; j++) {
-        analogWrite(pins[j],((float)i/31)*((float)PWM/100)*255);
-        }
-        delay(SMALL_DELAY);
-  }
-
-  for(uint8_t i=31; i>0; i--) {
-      for(uint8_t j=0; j<len_pins; j++) {
-        analogWrite(pins[j],(((float)i-1.0)/31)*((float)PWM/100)*255);
-        }
-        delay(SMALL_DELAY);
-  }
-  delay(SLEEP_INTERVAL);
 }
