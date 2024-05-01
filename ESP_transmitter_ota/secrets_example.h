4a54d3dc73e7ecc63084cd024b4629526d426b65
Leonardo Rivera <leodrivera@gmail.com>
2024-05-01 11:39:37 -0300

    Add secrets_example

---
+++ ESP_transmitter_ota/secrets_example.h
@@ -0,0 +1,12 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+const char *WLAN_SSID = ;  // Your ROUTER SSID
+const char *WLAN_PASS = ; // and WiFi PASSWORD
+const char* OTA_PASS = ; // OTA password
+
+// MQTT
+const char* MQTT_SERVER = ; // MQTT Server and Credentials
+const int MQTT_SERVERPORT = ; // MQTT port
+const char* MQTT_USERNAME = ; // MQTT user
+const char* MQTT_PASS = ; // MQTT password
